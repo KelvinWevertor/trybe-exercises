6806b9874970c7941c40924c20510bcecb05e37f
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-04-27 23:09:10 -0300

    Exercícios do Bloco 4.2 JavaScript - Array e loop For Trybe

---
+++ exercises/4.2/introducao-a-javascript-e-logica-de-programacao_2/Estrutura-de-Repeticao-For/script.js
@@ -0,0 +1,14 @@
+// Exercício 1 for
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
+// Exercício 2 for/of
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let pessoas of names) {
+    console.log(pessoas);
+}
